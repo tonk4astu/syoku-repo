bbc4c6b82a67dae16fbcf4e3da4e06a0e5fcee10
tonk4atu <norimaki229@gmail.com>
2023-09-20 11:45:13 +0900

    supabaseClient 追加

---
+++ app/components/login.ts
@@ -0,0 +1,10 @@
+import supabase from "@/app/components/supabaseClient";
+
+const login = async (email: string, password: string) => {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error) throw error;
+  return data;
+}
